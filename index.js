f5fea53773a22fddc13e7feab1a72dfabd6dceb2
Victiniiiii <victinii494@gmail.com>
2025-01-21 23:34:07 +0300

    prettier everything!!!!!!

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 // index.js
 
-const { app, BrowserWindow, Menu } = require('electron');
-const path = require('path');
+const { app, BrowserWindow, Menu } = require("electron");
+const path = require("path");
 const taratorFolder = __dirname;
 
 function createWindow() {
-    const mainWindow = new BrowserWindow({
-        width: 1600,
-        height: 850,
-        title: "TaratorMusic",
-        icon: path.join(taratorFolder, 'thumbnails/tarator16_icon.png'),
-        webPreferences: {
-            contextIsolation: false,
-            nodeIntegration: true,
-            additionalArguments: ['Content-Security-Policy', "script-src 'self'"]
-        }
-    });
+	const mainWindow = new BrowserWindow({
+		width: 1600,
+		height: 850,
+		title: "TaratorMusic",
+		icon: path.join(taratorFolder, "thumbnails/tarator16_icon.png"),
+		webPreferences: {
+			contextIsolation: false,
+			nodeIntegration: true,
+			additionalArguments: ["Content-Security-Policy", "script-src 'self'"],
+		},
+	});
 
-    mainWindow.loadFile('index.html');    
+	mainWindow.loadFile("index.html");
 }
 
 app.whenReady().then(() => {
-    Menu.setApplicationMenu(null); // ( removes the bar at the top )
-    app.setName("TaratorMusic");
-    createWindow();
+	Menu.setApplicationMenu(null); // ( removes the bar at the top )
+	app.setName("TaratorMusic");
+	createWindow();
 
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow();
-    });
+	app.on("activate", () => {
+		if (BrowserWindow.getAllWindows().length === 0) createWindow();
+	});
 });
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit();
+app.on("window-all-closed", () => {
+	if (process.platform !== "darwin") app.quit();
 });
