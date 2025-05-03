b174f4f7d4d51822804974af0dc8d643886ce0f4
Victiniiiii <victinii494@gmail.com>
2025-05-04 00:42:05 +0300

    upgraded the RPC script so it starts and stops working with the toggle

--- discord.js
+++ discord.js
@@ -1,16 +1,18 @@
-const clientId = "1258898699816275969"; // not a private key, just the ID of my app
+const clientId = "1258898699816275969";
 const DiscordRPC = require("discord-rpc");
-const RPC = new DiscordRPC.Client({ transport: "ipc" });
-DiscordRPC.register(clientId);
 
+let RPC = null;
 let discordApi = localStorage.getItem("discordApi") === "true";
 document.getElementById("toggleSwitchDiscord").checked = discordApi;
 
-if (discordApi) {
+function createRPC() {
+	RPC = new DiscordRPC.Client({ transport: "ipc" });
+	DiscordRPC.register(clientId);
+
 	RPC.on("ready", async () => {
 		updateDiscordPresence();
 	});
-    
+
 	RPC.login({ clientId }).catch((err) => {
 		console.error("Discord RPC Login Failed:", err);
 		document.getElementById("mainmenudiscordapi").innerHTML = "Discord API Status: Error";
@@ -18,15 +20,39 @@ if (discordApi) {
 	});
 }
 
-function toggleDiscordAPI() {
-	discordApi = !discordApi;
-	localStorage.setItem("discordApi", discordApi.toString());
+function destroyRPC() {
+	if (RPC) {
+		try {
+			RPC.clearActivity();
+			RPC.destroy();
+		} catch (err) {
+			console.error("Error while destroying RPC:", err);
+		}
+		RPC = null;
+	}
+}
+
+if (discordApi) {
+	createRPC();
+} else {
 	updateDiscordPresence();
 }
 
-async function updateDiscordPresence() {
+function toggleDiscordAPI() {
+	discordApi = !discordApi;
+	localStorage.setItem("discordApi", discordApi.toString());
+
 	if (discordApi) {
-		if (!RPC || !RPC.transport) {
+		createRPC();
+	} else {
+		destroyRPC();
+		updateDiscordPresence();
+	}
+}
+
+async function updateDiscordPresence() {
+	if (discordApi && RPC) {
+		if (!RPC.transport || RPC.transport.socket?.destroyed) {
 			document.getElementById("mainmenudiscordapi").innerHTML = "Discord API Status: Down";
 			document.getElementById("mainmenudiscordapi").style.color = "red";
 			return;
@@ -58,5 +84,3 @@ async function updateDiscordPresence() {
 		document.getElementById("mainmenudiscordapi").style.color = "yellow";
 	}
 }
-
-updateDiscordPresence();
