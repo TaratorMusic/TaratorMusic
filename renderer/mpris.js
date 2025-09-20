d31d3bcfee9b0e1f57aec59aaf7969544bac1ed1
Victiniiiii <victinii494@gmail.com>
2025-09-20 20:58:09 +0300

    added mpris support to linux

---
+++ renderer/mpris.js
@@ -0,0 +1,45 @@
+const Player = require("mpris-service");
+
+const player = Player({
+	name: "TaratorMusic",
+	identity: "TaratorMusic",
+	supportedUriSchemes: ["file"],
+	supportedMimeTypes: ["audio/mpeg", "application/ogg"],
+	supportedInterfaces: ["player"],
+});
+
+player.on("quit", function () {
+	process.exit();
+});
+
+const events = ["raise", "quit", "next", "previous", "pause", "playpause", "stop", "play", "seek", "position", "open", "volume", "loopStatus", "shuffle"];
+
+events.forEach(eventName => {
+	player.on(eventName, (...args) => {
+		if (eventName === "raise") console.log("unused function for now:", eventName);
+		else if (eventName === "quit") process.exit();
+		else if (eventName === "next") playNextSong();
+		else if (eventName === "previous") playPreviousSong();
+		else if (eventName === "pause") playPause("pause");
+		else if (eventName === "playpause") playPause("play");
+		else if (eventName === "stop") stopMusic();
+		else if (eventName === "play") playPause("play");
+		else if (eventName === "seek") console.log("unused function for now:", eventName);
+		else if (eventName === "position") console.log("unused function for now:", eventName); // Needs to be used to set song position
+		else if (eventName === "open") console.log("unused function for now:", eventName);
+		else if (eventName === "volume") console.log("unused function for now:", eventName); // Needs to be used to set volume
+		else if (eventName === "loopStatus") toggleLoop();
+		else if (eventName === "shuffle") toggleShuffle();
+	});
+});
+
+function editMPRIS(song) {
+	const row = musicsDb.prepare("SELECT song_name, song_length, thumbnail_extension, artist FROM songs WHERE song_id = ?").get(song);
+	player.metadata = {
+		"mpris:trackid": player.objectPath("track/0"),
+		"mpris:length": row.song_length,
+		"mpris:artUrl": path.join(thumbnailFolder, song + "." + row.thumbnail_extension),
+		"xesam:title": row.song_name,
+		"xesam:artist": [row.artist],
+	};
+}
